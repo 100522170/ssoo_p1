7c967c70a191219cf99e8e0b3d68a3a800ce4857
julio <chispa.jc8@gmail.com>
2026-02-20 05:02:56 +0000

    mycalc updateado con primeros pasos, aun falta comentar  esta bastante pobre

--- p1_calls/mycalc.c
+++ p1_calls/mycalc.c
@@ -1,9 +1,42 @@
 #include <fcntl.h> /* To use open, read, write, close */
+#include <string.h>
+#include <unistd.h> /*To use write*/
 
 const char *log_file = "mycalc.log";
-
 int main(int argc, char *argv[]) {
+  (void)argv;
+  char *error_msg = "Error: the arguments must be between 2 and 3\n";
 
+  /*First we check if the user used from 2 to 3 arguments, more or less will
+  raise an error because is not calculator mode neither history mode*/
+
+  if (argc < 3 || argc > 4) {
+
+    if (write(2, error_msg, strlen(error_msg)) < 0) {
+      return -1;
+    }
+
+    return -1;
+  }
+
+  char *history_mode = "Entrando en modo historial";
+  char *calc_mode = "Entrando en modo historial";
+  if (argc == 3) {
+
+    if (strcmp(argv[1], "-b") == 0) {
+
+      if (write(1, history_mode, strlen(history_mode)) < 0) {
+        return 1;
+      }
+    }
+  }
+
+  if (argc == 4) {
+
+    if (write(1, calc_mode, strlen(calc_mode)) < 0) {
+      return 0;
+    }
+  }
   /* Complete */
 
   return 0;
--- p1_calls/mydu.c
+++ p1_calls/mydu.c
@@ -1,8 +1,9 @@
 /* name of the binary file to write/read */
 const char *binary_file = "mydu.bin";
 
-
 int main(int argc, char *argv[]) {
+  (void)argv;
+  (void)argc;
 
   /* Complete */
 
